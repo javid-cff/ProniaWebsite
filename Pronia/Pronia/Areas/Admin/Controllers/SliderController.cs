94699d8062c19dc302b9a7ae1dfccad900ff05bc
javid-cff <javidrm-mpa101@code.edu.az>
2025-12-23 05:24:13 +0400

    Pronia Website is committed.

---
+++ Pronia/Pronia/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pronia.Contexts;
+
+namespace Pronia.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController(ProniaDbContext _context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var sliders = await _context.Sliders.ToListAsync();
+            return View(sliders);
+        }
+    }
+}
---
+++ Pronia/Pronia/Contexts/ProniaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.Models;
+
+namespace Pronia.Contexts
+{
+    public class ProniaDbContext : DbContext
+    {
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public ProniaDbContext(DbContextOptions<ProniaDbContext> options) : base(options)
+        {
+        }
+    }
+}
